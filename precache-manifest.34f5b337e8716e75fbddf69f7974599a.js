46992337f2b9d77843b311e7d146cf51b7ec8006
runitrupam <runitranjankumar@gmail.com>
2023-11-02 01:04:26 +0530

    Updates

---
+++ precache-manifest.34f5b337e8716e75fbddf69f7974599a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93711ae493eb01b57d0d9b779d326c4",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "73f8eb1fcb6c72db3b93",
+    "url": "/my-portfolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "5fdddfcc58fde02137e4",
+    "url": "/my-portfolio/static/css/main.9747798b.chunk.css"
+  },
+  {
+    "revision": "73f8eb1fcb6c72db3b93",
+    "url": "/my-portfolio/static/js/2.392f7634.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/my-portfolio/static/js/2.392f7634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdddfcc58fde02137e4",
+    "url": "/my-portfolio/static/js/main.32b70115.chunk.js"
+  },
+  {
+    "revision": "f8bc21605bac90413ed8",
+    "url": "/my-portfolio/static/js/runtime-main.cc7e5888.js"
+  }
+]);
